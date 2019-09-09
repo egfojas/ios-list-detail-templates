86dae13c3aeb57b95023c1e1b9e9e4b1becb3c02
Edgar Allan Fojas <edgar@circles.asia>
2019-09-09 16:46:47 +0800

    create single-view app project

---
+++ ios-list-detail-templates/ios-list-detail-templates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-list-detail-templates
+//
+//  Created by Edgar Allan Fojas on 9/9/19.
+//  Copyright © 2019 Edgar Allan Fojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
